b0b5a1626fbb8e68fd6e29445c13626d11f6391f
Maria <eberle.4944@aluno.pr.senac.br>
2024-07-18 21:51:00 -0300

    ADD TesteSub

--- TesteSub.py
+++ TesteSub.py
@@ -1,24 +1,23 @@
-def retorna_subtração(lista):
-    if len(lista) == 0:
-        return "0"
+def retorna_subtração(num1,num2):
+    return num1-num2
 
 import unittest
 
 class testRetornaSubtração(unittest.TestCase):
     def test_retorna_subtração_vazia(self):
-        self.assertEqual(retorna_subtração([]), "0")
+        self.assertEqual(retorna_subtração(2,2),0)
         
     def test_subtraçao(self):
-        self.assertEqual(retorna_subtração([11, 6], 5))
+        self.assertEqual(retorna_subtração(11, 6), 5)
     
     def test_subtraçao_negativa(self):
-        self.assertEqual(retorna_subtração([-11, 6], -17))    
+        self.assertEqual(retorna_subtração(-11, 6), -17)    
         
     def test_subtraçao_com_tres_numeros(self):
-        self.assertEqual(retorna_subtração([11, 6, 4], 1))     
+        self.assertEqual(retorna_subtração( 6, 4), 2)     
         
     def test_subtraçao_com_tres_numeros_negativos(self):
-        self.assertEqual(retorna_subtração([-11, -6, -4], -1))
+        self.assertEqual(retorna_subtração(-11, -6,), -5)
 
 if __name__ == '__main__':
     unittest.main()       
